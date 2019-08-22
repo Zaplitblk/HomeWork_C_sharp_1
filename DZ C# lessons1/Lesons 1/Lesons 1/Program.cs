eadae3ea5df236f994e7b90ec62805bf85869efc
Nikita Maksimov <zaplitblk@gmail.com>
2019-08-22 19:37:03 +0300

    1

--- DZ C# lessons1/Lesons 1/Lesons 1/Program.cs
+++ DZ C# lessons1/Lesons 1/Lesons 1/Program.cs
@@ -6,7 +6,7 @@ namespace Lesons_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!1");
         }
     }
 }
